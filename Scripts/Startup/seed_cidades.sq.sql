49e29f4d142d62ee440f7aabea30e377f200ebbc
Thaísla Paim dos Santos Batista <thaisla.batista@aluno.senai.br>
2022-08-19 21:07:01 -0300

    backup

---
+++ Scripts/Startup/seed_cidades.sq.sql
@@ -0,0 +1,4 @@
+INSERT INTO endereco_alunos  (logradouro, numero, bairro, cep, uf_id, municipio_id)
+VALUES 
+('Rua Tiradentes', 9658, 'Interlagos', 29.900-999, 244, 9),
+('Avenida Vasco Fernandes Coutinho', 1190, 'Canivete', 19.365-968, 252, 1)
