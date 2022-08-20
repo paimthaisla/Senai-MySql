42f68da41f958188d17e061c2acffee0ed6f9515
Thaísla Paim dos Santos Batista <thaisla.batista@aluno.senai.br>
2022-08-19 21:12:33 -0300

    backup

---
+++ Scripts/Startup/seed_ufs.sq.sql
@@ -0,0 +1,12 @@
+INSERT INTO cidades ( nome)
+VALUES ('Afonso Claudio'),
+('Água Doce do Norte'),
+('Águia Branca'),
+('Alegre'),
+('Alfredo Chaves'),
+('Alto Rio Novo'),
+('Anchieta'),
+('Apiacá'),
+('Aracruz'),
+('Atílio Vivácqua')
+
