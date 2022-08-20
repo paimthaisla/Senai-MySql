4de512bf0fa5d1a21754b4ef9d06481cab129ebe
Thaísla Paim dos Santos Batista <thaisla.batista@aluno.senai.br>
2022-08-19 21:16:38 -0300

    deletando arquivos duplicados

--- seed_endereco_aluno.sql.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO wwonline12.endereco_alunos (logradouro,numero,bairro,cep,uf_id,municipio_id) VALUES
-	 ('Rua Tiradentes','9658','Interlagos','-969.100',244,9),
-	 ('Avenida Vasco Fernandes Coutinho','1190','Canivete','-948.635',252,1);
